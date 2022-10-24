6b0d1725e2df049cc6060d10e2ca9712129407c2
AhmadAlharbi <ahmaadzaid7@gmail.com>
2022-10-24 11:20:41 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        DarkSpaceBlue: "#00001A",
+        AlmostWhite: "#FFFDFA",
+        Gunmetal: "#5E607A",
+        LightVermillion: "#F15D51",
+        Silver: "#C5C6CE",
+        Yellow: "#E9AA52",
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      },
+      letterSpacing: {
+        widest: ".25em",
+      },
+    },
+  },
+  plugins: [],
+};
